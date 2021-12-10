510d6bce29dee6867b43f1be2e84620e3fffe830
Zhongshi Luo <752828402@qq.com>
2021-12-09 21:37:15 -0500

    Feature: start panel partially accomplished

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -9,10 +9,100 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class GameDriver {
     public static void main(String[] args) {
-        GameFrame frame = new GameFrame("Mine Sweeper");
-        frame.setVisible(true);
+        SettingDialog dialog = new SettingDialog();
+    }
+}
+
+/*
+The dialog of setting panel, it offers three preset: easy, intermediate, difficult and
+also allows players to customize the number of row, column and mine.
+ */
+class SettingDialog extends JDialog{
+
+    public SettingDialog() {
+        setTitle("Settings");
+        setSize(400, 300);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+        setLocation(x, y);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setAlwaysOnTop(true);
+        setLayout(null);
+        setResizable(false);
+        setVisible(true);
+
+        //add elements
+        JLabel chooseDiff = new JLabel("Preset Difficulty:");
+        chooseDiff.setBounds(20, 15, 105,20);
+        chooseDiff.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.add(chooseDiff);
+
+        JButton easy = new JButton("Easy");
+        easy.setBounds(20,45, 100, 30);
+        easy.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        easy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new GameFrame(9, 14, 15);
+                dispose();
+            }
+        });
+        this.add(easy);
+
+        JButton intermediate = new JButton("Intermediate");
+        intermediate.setBounds(145,45, 100, 30);
+        intermediate.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        intermediate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new GameFrame(15, 20, 40);
+                dispose();
+            }
+        });
+        this.add(intermediate);
+
+        JButton expert = new JButton("Expert");
+        expert.setBounds(270,45, 100, 30);
+        expert.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        expert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new GameFrame(20, 30, 100);
+                dispose();
+            }
+        });
+        this.add(expert);
+
+        JLabel custom = new JLabel("Custom:");
+        custom.setBounds(20, 100, 60,20);
+        custom.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.add(custom);
+
+        JLabel row = new JLabel("Rows:");
+        row.setBounds(50, 130, 45,20);
+        row.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.add(row);
+
+        JLabel col = new JLabel("Columns:");
+        col.setBounds(165, 130, 60,20);
+        col.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.add(col);
+
+        JLabel mine = new JLabel("Mines:");
+        mine.setBounds(295, 130, 45,20);
+        mine.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.add(mine);
+
+        JTextField rowText = new JTextField(10);
+        rowText.setBounds(50, 140, 45, 20);
+        this.add(rowText);
+
     }
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,20 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-//TODO: Won't die in the first move; Mines remain; setting panel
+//TODO: Mines remain; setting panel
 
+/**
+ * The toppest container of the game window, a JFrame, which contains the game board
+ * and a bottom bar with reset button and info about the mines remain. Before game start
+ * there will pop up a dialog to choose the difficulty or customize the board.
+ */
 public class GameFrame extends JFrame {
 
-    public GameBoard board;
+    private GameBoard board;
 
-    public GameFrame(String title){
-        super(title);
-        this.setDefaultCloseOperation(3);//EXIT_ON_CLOSE
-//        Container container = frame.getContentPane();
+    public GameFrame(int row, int col, int mine){
+        this.setTitle("Mine Sweeper");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);//EXIT_ON_CLOSE
         this.setLocationRelativeTo(null);
-        board = new GameBoard(5, 5);
+        this.setResizable(false);
 
+        //pop up the setting dialog and get the input info
+//        SettingDialog dialog = new SettingDialog();
+//        dialog.setAlwaysOnTop(true);
 
+        //initialize the panel
+        board = new GameBoard(row, col, mine);
+        this.setSize(board.getBoardWidth() + 14, board.getBoardHeight()+ 64);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+        this.setLocation(x, y);
+
+        //initialize the bottom bar with a reset button and text of mines remain
         JPanel bottomBar = new JPanel(new BorderLayout());//to contain the three components: reset button, number of mines remain and time elapsed.
         JButton reset = new JButton("RESET");
         reset.addMouseListener(new MouseAdapter() {
@@ -24,21 +40,18 @@ public class GameFrame extends JFrame {
                 reset();
             }
         });
+        reset.setBackground(Color.lightGray);
+        bottomBar.setBackground(Color.lightGray);
         JLabel mines = new JLabel("MINES REMAIN:");
-
         bottomBar.add(reset, BorderLayout.EAST);
         bottomBar.add(mines, BorderLayout.WEST);
 
-        this.setSize(board.getBoardWidth() + 14, board.getBoardHeight()+ 64);
-
-
-//        frame.getContentPane().add(board);
+        //add the components to the frame
         this.setLayout(new BorderLayout());
         this.add(board, BorderLayout.CENTER);
         this.add(bottomBar, BorderLayout.SOUTH);
 
-        this.setResizable(false);
-        this.setVisible(true);
+        setVisible(true);
     }
 
     private void reset(){
@@ -51,3 +64,6 @@ public class GameFrame extends JFrame {
         repaint();
     }
 }
+
+
+
